a9dc1256b70871e69d3bb23fd027dfb373b7b91a
EC2 Default User <ec2-user@ip-172-31-29-80.ap-northeast-1.compute.internal>
2023-06-12 16:14:47 +0000

    [Finish]アプリケーションを完成させよう１１

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,10 @@ class ListsController < ApplicationController
   end
 
   def create
-    # 1.&2.データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3.データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 3.トップ画面へリダイレクト
-    redirect_to '/top'
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -18,14 +16,28 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
